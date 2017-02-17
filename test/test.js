497c5dd290abf08f73053ca6db0b20f5ff3eff17
thadeuszlay <thadeuszlay@gmail.com>
2017-02-17 21:25:33 +0100

    cover the case with more than 2 letter numbers

--- test/test.js
+++ test/test.js
@@ -57,6 +57,15 @@ describe('Roman Numbers to Arabic Numbers', function () {
         it('should return 6 for VI', function () {
           assert.equal(6, romantoarabic('VI'));
         });
+        it('should return 20 for XX', function () {
+          assert.equal(20, romantoarabic('XX'));
+        });
+        it('should return 60 for LX', function () {
+          assert.equal(60, romantoarabic('LX'));
+        });
+        it('should return 600 for DC', function () {
+          assert.equal(600, romantoarabic('DC'));
+        });
       });
     describe('subtracting (2nd number is larger than 1st number)',
       function () {
@@ -66,17 +75,35 @@ describe('Roman Numbers to Arabic Numbers', function () {
         it('should return 9 for IX', function () {
           assert.equal(9, romantoarabic('IX'));
         });
+        it('should return 40 for XL', function () {
+          assert.equal(40, romantoarabic('XL'));
+        });
         it('should return 49 for IL', function () {
           assert.equal(49, romantoarabic('IL'));
         });
         it('should return 90 for XC', function () {
           assert.equal(90, romantoarabic('XC'));
         });
+        it('should return 900 for CM', function () {
+          assert.equal(900, romantoarabic('CM'));
+        });
       });
   });
   describe('Basic Combinations with more than two letter numbers', function () {
     it('should return 3 for III', function () {
       assert.equal(3, romantoarabic('III'));
+    });
+    it('should return 7 for VII', function () {
+      assert.equal(7, romantoarabic('VII'));
+    });
+    it('should return 8 for VIII', function () {
+      assert.equal(8, romantoarabic('VIII'));
+    });
+    it('should return 1984 for MCMLXXXIV', function () {
+      assert.equal(1984, romantoarabic('MCMLXXXIV'));
+    });
+    it('should return 1876 for MDCCCLXXVI', function () {
+      assert.equal(1876, romantoarabic('MDCCCLXXVI'));
     });
   });
 });
@@ -128,6 +155,14 @@ function romantoarabic(romanNumber) {
     }
     return basicNumber(romanNumber[1]) + basicNumber(romanNumber[0]);
   } else if (romanNumberLength > 2) {
-    return 3;
+    let translatedArabicNumber = 0;
+    for (let i = romanNumberLength - 1; i >= 0; i--) {
+      if (basicNumber(romanNumber[i + 1]) > basicNumber(romanNumber[i])) {
+        translatedArabicNumber -= basicNumber(romanNumber[i]);
+      } else {
+        translatedArabicNumber += basicNumber(romanNumber[i]);
+      }
+    }
+    return translatedArabicNumber;
   }
 }
