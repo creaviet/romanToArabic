0513ba2ec8b9b736a87174e93e119bba922027f1
thadeuszlay <thadeuszlay@gmail.com>
2017-02-17 20:39:07 +0100

    refactor

--- test/test.js
+++ test/test.js
@@ -1,53 +1,133 @@
+//'esversion: 6'
+//
 var assert = require('assert');
 describe('Roman Numbers to Arabic Numbers', function () {
-  describe('Roman Numbers', function () {
-    it('should return 1 for I', function () {
-      assert.equal(1, romantoarabic('I'));
+  describe('One letter Numbers and basic inputs', function () {
+    describe('Basic numbers', function () {
+      it('should return 1 for I', function () {
+        assert.equal(1, romantoarabic('I'));
+      });
+      it('should return 5 for V', function () {
+        assert.equal(5, romantoarabic('V'));
+      });
+      it('should return 10 for X', function () {
+        assert.equal(10, romantoarabic('X'));
+      });
+      it('should return 50 for L', function () {
+        assert.equal(50, romantoarabic('L'));
+      });
+      it('should return 100 for C', function () {
+        assert.equal(100, romantoarabic('C'));
+      });
+      it('should return 500 for D', function () {
+        assert.equal(500, romantoarabic('D'));
+      });
+      it('should return 1000 for M', function () {
+        assert.equal(1000, romantoarabic('M'));
+      });
     });
-    it('should return 5 for V', function () {
-      assert.equal(5, romantoarabic('V'));
+    describe('Faulty input', function () {
+      it('should return undefined if argument is missing',
+        function () {
+          assert.equal(undefined, romantoarabic());
+        });
+      it('should return undefined if argument is null',
+        function () {
+          assert.equal(undefined, romantoarabic(null));
+        });
+      it(
+        'should return undefined for non-roman-number-letters, e.g. t',
+        function () {
+          assert.equal(undefined, romantoarabic('t'));
+        });
+      it(
+        'should return undefined for non-roman-number-letters, e.g. 8',
+        function () {
+          assert.equal(undefined, romantoarabic('8'));
+        });
     });
-    it('should return 10 for X', function () {
-      assert.equal(10, romantoarabic('X'));
-    });
-    it('should return 50 for L', function () {
-      assert.equal(50, romantoarabic('L'));
-    });
-    it('should return 100 for C', function () {
-      assert.equal(100, romantoarabic('C'));
-    });
-    it('should return 500 for D', function () {
-      assert.equal(500, romantoarabic('D'));
-    });
-    it('should return 1000 for M', function () {
-      assert.equal(1000, romantoarabic('M'));
-    });
+
   });
-  describe('Basic Combinations', function () {
-    it('should return 2 for II', function () {
-      assert.equal(2, romantoarabic('II'));
+  describe('Basic Combinations with two letter numbers', function () {
+    describe('adding (2nd number is smaller or equal than 1st number)',
+      function () {
+        it('should return 2 for II', function () {
+          assert.equal(2, romantoarabic('II'));
+        });
+        it('should return 6 for VI', function () {
+          assert.equal(6, romantoarabic('VI'));
+        });
+      });
+    describe('subtracting (2nd number is larger than 1st number)',
+      function () {
+        it('should return 4 for IV', function () {
+          assert.equal(4, romantoarabic('IV'));
+        });
+        it('should return 9 for IX', function () {
+          assert.equal(9, romantoarabic('IX'));
+        });
+        it('should return 49 for IL', function () {
+          assert.equal(49, romantoarabic('IL'));
+        });
+        it('should return 90 for XC', function () {
+          assert.equal(90, romantoarabic('XC'));
+        });
+      });
+  });
+  describe('Basic Combinations with more than two letter numbers', function () {
+    it('should return 3 for III', function () {
+      assert.equal(3, romantoarabic('III'));
     });
   });
 });
 
+function basicNumber(romanNumber) {
+  let returnValue = null;
+  switch (romanNumber) {
+  case 'I':
+    returnValue = 1;
+    break;
+  case 'V':
+    returnValue = 5;
+    break;
+  case 'X':
+    returnValue = 10;
+    break;
+  case 'L':
+    returnValue = 50;
+    break;
+  case 'C':
+    returnValue = 100;
+    break;
+  case 'D':
+    returnValue = 500;
+    break;
+  case 'M':
+    returnValue = 1000;
+    break;
+  default:
+    returnValue = undefined;
+    break;
+  }
+  return returnValue;
+}
+
 function romantoarabic(romanNumber) {
-  if (romanNumber === 'I') {
-    return 1;
+  let translatedArabicNumber = null;
+  let arabicNumberArray = [];
+  let operationsArray = [];
+  if (!romanNumber) {
+    return undefined;
   }
-  if (romanNumber === 'V') {
-    return 5;
+  let romanNumberLength = romanNumber.length;
+  if (romanNumberLength === 1) {
+    return basicNumber(romanNumber);
+  } else if (romanNumberLength === 2) {
+    if (basicNumber(romanNumber[1]) > basicNumber(romanNumber[0])) {
+      return basicNumber(romanNumber[1]) - basicNumber(romanNumber[0]);
+    }
+    return basicNumber(romanNumber[1]) + basicNumber(romanNumber[0]);
+  } else if (romanNumberLength > 2) {
+    return 3;
   }
-  if (romanNumber === 'X') {
-    return 10;
-  }
-  if (romanNumber === 'L') {
-    return 50;
-  }
-  if (romanNumber === 'C') {
-    return 100;
-  }
-  if (romanNumber === 'D') {
-    return 500;
-  }
-  return 1000;
 }
